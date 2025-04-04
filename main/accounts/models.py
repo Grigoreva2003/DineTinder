28555b5a64f819a2c5062c6de79e1c9e9e9cd934
vagrigoreva <grigvasilisa@gmail.com>
2025-04-04 23:51:22 +0300

    add user model & configure authorization

--- main/accounts/models.py
+++ main/accounts/models.py
@@ -6,6 +6,9 @@ class User(models.Model):
     sex = models.CharField(max_length=10)
     email = models.EmailField(unique=True)
 
+    access_token = models.TextField(blank=True, null=True)
+    refresh_token = models.TextField(blank=True, null=True)
+
     def __str__(self):
         return f"{self.name} ({self.sex}) - {self.email}"
 
